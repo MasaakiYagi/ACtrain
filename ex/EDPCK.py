98b89f21d85ded9fcd2351cbcf0aed5d20a03728
YaGitHub <eufo312@gmail.com>
2021-08-28 23:27:50 +0900

    20210828-2

--- ex/EDPCK.py
+++ ex/EDPCK.py
@@ -4,18 +4,18 @@
 n, k = list(map(int, input().split()))
 a = list(map(int, input().split()))
 
-# dp[i]残りの石がiのとき，最適戦略で終了するまでの手数　0のとき終わってる
-dp = [0]*(k+1)
+# dp[i]残りの石がiのとき，直後の手の人が勝てるか（True/False）
+dp = [True]*(k+1)
 for i in range(k+1):
     if i == 0 or i < min(a):
-        dp[i] = 0
+        dp[i] = False
     else:
         # i-a[j]が0以上のaを抽出
         # min(dp[i-a])+1がdp[i]の値となる
         dp_dash = [dp[i-j] for j in a if i-j>=0]
-        dp[i] = min(dp_dash)+1
+        dp[i] = not(all(dp_dash))
 
-if dp[k]%2 == 0:
-    print("Second")
-else:
+if dp[k]:
     print("First")
+else:
+    print("Second")
