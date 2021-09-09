2ba82c1e480c275e3c51c52e2dec35d9ba8728ec
YaGitHub <eufo312@gmail.com>
2021-09-09 21:33:22 +0900

    20210909

--- ex/典型90問016.py
+++ ex/典型90問016.py
@@ -2,5 +2,17 @@
 # https://atcoder.jp/contests/typical90/tasks/typical90_p
 
 n = int(input())
-a, b, c = list(map(int, input().split()))
+a = list(map(int, input().split()))
 
+a = sorted(a, reverse=True)
+
+# 全探索（ただし，3重ループではなく2重）
+ans = 9999
+for i in range(int(n/a[0])+2):
+    for j in range(int((n-i*a[0])/a[1])+2):
+        if n >= (i*a[0]+j*a[1]):
+            q = (n-(i*a[0]+j*a[1]))//a[2]
+            mod = (n-(i*a[0]+j*a[1]))%a[2]
+            if mod == 0 and ans>(q+i+j):
+                ans = q+i+j
+print(ans)
