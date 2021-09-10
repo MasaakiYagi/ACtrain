674e7354ecdb8f1d2a9e98928d066347a2d30749
YaGitHub <eufo312@gmail.com>
2021-09-10 21:44:09 +0900

    20210910

--- ex/典型90問018.py
+++ ex/典型90問018.py
@@ -2,7 +2,6 @@
 # https://atcoder.jp/contests/typical90/tasks/typical90_r
 
 import math
-import numpy as np
 
 t = int(input())
 l, x, y = list(map(int, input().split()))
@@ -10,32 +9,17 @@ q = int(input())
 es = [int(input()) for _ in range(q)]
 
 # 像の位置
-tk = np.array([x,y,0])
+tk = [x,y,0]
 for e in es:
     # 観覧車の座標計算
-    pos = np.array([
+    pos = [
         0,
         -l/2*math.sin(2*math.pi*e/t),
         l/2-l/2*math.cos(2*math.pi*e/t)
-    ])
-    # 観覧車~像ベクトル計算
-    tkkn = tk-pos
-    # 観覧車から像方向への水平ベクトル
-    knhor = np.copy(tkkn)
-    knhor[2] = 0
-    # tkknとknhorの内積
-    d = np.dot(knhor,tkkn)
-    # tkknとknhorの絶対値積
-    dd = np.linalg.norm(tkkn)*np.linalg.norm(knhor)
-    # 回答ラジアン
-    if d>dd:
-        dd=d
-    rad = math.acos(d/dd)
-    deg = np.rad2deg(rad)
-    
-    if deg<0:
-        deg = abs(deg)
-    while(deg>90):
-        deg -= 90
+    ]
+    # 観覧車~像水平距離
+    hor = math.sqrt(tk[0]**2+(tk[1]-pos[1])**2)
+    rad = math.atan(pos[2]/hor)
+    deg = math.degrees(rad)
     
     print(deg)
