a344eb8621e444bd3b91d77f1f3a17577e6756fd
YaGitHub <eufo312@gmail.com>
2021-09-16 00:08:41 +0900

    20210916

--- ex/典型90問064.py
+++ ex/典型90問064.py
@@ -6,22 +6,24 @@ a = list(map(int, input().split()))
 v = [list(map(int, input().split())) for _ in range(q)]
 
 
-# 初期不便さ計算
-m = sum(a)/len(a)
-da = list(map(lambda i:abs(i-m), a))
-suma = int(sum(da)*2-da[0]-da[-1])
+# 階差配列bを作成b[i] = a[i+1]-a[i]
+b = []
+suma = 0
+for i in range(n-1):
+    b.append(a[i+1]-a[i])
+    suma += abs(b[i])
 
-for i in range(len(v)):
-    # 変動境界の不便さをsumaから引く
+# 地殻変動更新
+for i in range(q):
+    dl = 0
+    dr = 0
     if v[i][0]>1:
-        suma -= abs(a[v[i][0]-1]-a[v[i][0]-2])
-    if v[i][1]<len(a):
-        suma -= abs(a[v[i][1]]-a[v[i][1]-1])
-    # 隆起
-    a[v[i][0]-1:v[i][1]] = list(map(lambda j:j+v[i][2], a[v[i][0]-1:v[i][1]]))
-    # 変動部分再計算
-    if v[i][0]>1:
-        suma += abs(a[v[i][0]-1]-a[v[i][0]-2])
-    if v[i][1]<len(a):
-        suma += abs(a[v[i][1]]-a[v[i][1]-1])
+        dl = -abs(b[v[i][0]-2])
+        b[v[i][0]-2] += v[i][2]
+        dl += abs(b[v[i][0]-2])
+    if v[i][1]<n:
+        dr = -abs(b[v[i][1]-1])
+        b[v[i][1]-1] -= v[i][2]
+        dr += abs(b[v[i][1]-1])
+    suma += dl+dr
     print(suma)
