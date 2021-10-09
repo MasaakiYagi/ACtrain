aacd78cea01c555cfb2f3cee66c498973ee9c882
YaGitHub <eufo312@gmail.com>
2021-10-09 18:55:15 +0900

    20211009

--- ex/tes.py
+++ ex/tes.py
@@ -9,26 +9,39 @@
 # import sys
 # sys.setrecursionlimit(10**8)
 
+from collections import deque
 
 n=int(input())
-days = []
-for _ in range(n):
-    a,b = list(map(int, input().split()))
-    days.append([a,1])
-    days.append([a+b,-1])
+tab = {0:"(",
+        1:")"}
 
-days.sort()
-
-ans = [0]*(n+2)
-s = days[0][0]
-num = 1
-
-for d in days:
-    ans[num] += d[0]-s
-    if d[1]==1:
-        num += 1
-    else:
-        num -= 1
-    s = d[0]
-
-print(*ans[2:])
+if n%2==0:  # 奇数は無視
+    for i in range(2**n):
+        bi = list(bin(i)[2:])
+        bi = list(map(int, bi))
+        bi = deque(bi)
+        sa = n-len(bi)
+        if sa>0:  # 桁数が足りなかったら補完
+            salist = [0]*sa
+            for j in salist:
+                bi.appendleft(j)
+        
+        # 正しいカッコか判定
+        cnt = 0
+        flag = 1
+        for j in bi:
+            if j==0:
+                cnt += 1
+            else:
+                cnt -= 1
+            
+            if cnt<0:
+                flag = 0
+                break
+            
+        if not(cnt==0):
+            flag = 0
+        
+        if flag:
+            ans = list(map(lambda v:tab[v], bi))
+            print("".join(ans))
