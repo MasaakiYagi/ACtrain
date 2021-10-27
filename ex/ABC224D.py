3f7f0c30ead0c9b5d738466b5afba83836b51eb7
YaGitHub <eufo312@gmail.com>
2021-10-27 21:36:14 +0900

    20211027

---
+++ ex/ABC224D.py
@@ -0,0 +1,9 @@
+# ABC224-D
+# https://atcoder.jp/contests/abc224/tasks/abc224_d
+
+
+
+m = int(input())
+e = [list(map(int, input().split())) for _ in range(m)]
+p = list(map(int, input().split()))
+
