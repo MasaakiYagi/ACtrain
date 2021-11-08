934f1e7de2564318d6491c5ce3581d6afaeafa53
YaGitHub <eufo312@gmail.com>
2021-11-08 23:28:28 +0900

    20211108

--- ex/tes.py
+++ ex/tes.py
@@ -13,36 +13,17 @@
 # from collections import deque
 
 n = int(input())
-a = list(map(int, input().split()))
+t = [0]*(n+1)
+e = [[] for _ in range(n+1)]
+for i in range(1,n+1):
+    x = list(map(int, input().split()))
+    for j in range(len(x)):
+        if j == 0:
+            t[i] = x[j]
+        elif j == 1:
+            k = x[j]
+        else:
+            e[i].append(x[j])
+            e[x[j]].append(i)
 
-# 状態保存DP
-audp = [{"num":0,"hist":[]} for _ in range(n+1)]
-agdp = [{"num":0,"hist":[]} for _ in range(n+1)]
-audp[0]["num"] = 1
-
-for i in range(n):
-    candau = audp[i].copy()
-    candag = agdp[i].copy()
-    # 金の最大値
-    if candau["num"]>candag["num"]/a[i]:
-        # 何もしない方がいいとき
-        audp[i+1]["num"] = candau["num"]
-        audp[i+1]["hist"] = candau["hist"].copy()
-        audp[i+1]["hist"].append(0)
-    else:
-        # 銀を交換した方がいいとき
-        audp[i+1]["num"] = candag["num"]/a[i]
-        audp[i+1]["hist"] = candag["hist"].copy()
-        audp[i+1]["hist"].append(1)
-    # 銀の最大値
-    if candag["num"]>candau["num"]*a[i]:
-        # 何もしない方がいいとき
-        agdp[i+1]["num"] = candag["num"]
-        agdp[i+1]["hist"] = candag["hist"].copy()
-        agdp[i+1]["hist"].append(0)
-    else:
-        # 金を交換した方がいいとき
-        agdp[i+1]["num"] = candau["num"]*a[i]
-        agdp[i+1]["hist"] = candau["hist"].copy()
-        agdp[i+1]["hist"].append(1)
-print(*audp[-1]["hist"])
+# 技nからbfsで葉までを探索，最も総時間の少ないルートの時間をansとする
